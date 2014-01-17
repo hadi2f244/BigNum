76d9a5bcf0bac10a4766933c36fa1b71749181fe
hadi2f244 <hadi2f244@ymail.com>
2014-01-17 14:17:07 -0800

    Update BNum.h

--- BNum.h
+++ BNum.h
@@ -6,17 +6,25 @@
 class BNum
 {
 private:
-    int size;
-    char *digits=new char[1]; //storing the digits of the num
+    //these initializions to avoid memory errors!
+    int size=1;
+    char *digits=new char; //storing the digits of the num
+    bool sign=true;//true --> positive & false --> negetive
     void removeZero(); //remove extra zero from left
+    BNum normalPlus(const BNum First,const BNum Second);//add twe positive BNums
+    BNum normalMinus(const BNum First,const BNum Second);//minus twe negetive BNums that we know result is positive
+    int normalCompare(const BNum First,const BNum Second);//compare twe positive BNum : (-1-->first<second) & (0-->equal) & (1-->first>second)
+    BNum normalDivide(const BNum First,const BNum Second);//devide twe positive BNums
+
 public:
     BNum();
-    BNum(QString str);
-    BNum(int Num);
-    BNum& operator=(const BNum& other); //copy constructor
+    BNum(const QString Str);
+    BNum(const int Num);
+    BNum(const BNum& other);//copy constructor
+    BNum& operator=(const BNum& other);
     ~BNum();
 
-    void operator=(const QString& str); //some times we need to change the num with str
+    void operator=(const QString& Str); //some times we need to change the num with str
     void operator=(const int& Num);//some times we need to change the num with int
     BNum operator++(int);//++
     BNum operator--(int);//>>
@@ -27,11 +35,11 @@ public:
 
     //why we use the & before other?!
     BNum operator +(const BNum &other);//+
-    BNum operator +(const int &other);
+    BNum operator +(const int Num);
     BNum operator -(const BNum &other);//-
+    BNum operator -(const int Num);
     BNum multByOneDigit(int d);
-
-    bool operator ==(const BNum &other);
+       bool operator ==(const BNum &other);
     bool operator <(const BNum &other);
     bool operator >(const BNum &other);
     bool operator <=(const BNum &other);
